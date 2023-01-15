ec6c6c7dab6b7314eea8e1a69ae4488d67717691
Seamus Mullan <24carrotgaming@gmail.com>
2023-01-15 20:39:00 +0000

    Fixed EQN x^2 + 47.5x / 205

--- Revision 0.py
+++ Revision 0.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ## Variables
-angle = np.deg2rad(23)      ## launch angle in degrees
+# angle = np.deg2rad(23)      ## launch angle in degrees
 g = 9.81                    ## gravity
 u = 18.775                  ## initial velocity (x and y components follow)
 ux = 17.277
@@ -10,37 +10,58 @@ uy = 7.345
 sy = 2.75                   ## height of target (2.5 - 3 meters)
 hSpread = 22                ## horizontal spread in degrees
 
+dist = 47.5                 ## distance to target (45 - 50 meters)
+
 ## Revision 0
 ## calculate motion of projectile (equation)
 xvals = []
 yvals = []
 
-def f(t):
-    return uy * t - (0.5 * g * t**2)
+def f(x):
+    return (-x**2 + 47.5*x)/205
 
-for t in np.arange(0, 10, 0.01):
-    x = ux * t
-    y = f(t)
-    if y < 0:
-        continue
+print(f(dist))
 
+maxX = 0
+maxY = 0
+
+for x in np.arange(0, 47.5, .01):
+    y = f(x)
     xvals.append(x)
     yvals.append(y)
+    if y > maxY:
+        maxY = y
+        maxX = x
+
+
+
+
+
+"""
+make the line formula then sub in every point from x=0 to x=47.5
+get perp dist from the point (x, g(x)) to the curve and add the distances
+change the line formula and repeat, noting the smallest value of theta
+return theta and the distance over the line to the curve
+"""
+
+## Line from player to max of eqn
+
+
+## iterate over points on line, find dist to curve
+
+
+## using smallest theta value, create a line and graph it on the plot
+
 
 
 ## plot 1
 plt.plot(xvals, yvals, 'r', label='Projectile Motion')
 plt.axhline(y=sy, color='b', linestyle="dotted", label='Max Height')
 plt.xlabel('Distance X (m)')
-plt.xticks(np.arange(0, 30, 2))
+# plt.xticks(np.arange(0, 30, 2))
 plt.ylabel('Distance Y (m)')
-plt.yticks(np.arange(0, 3, .25))
+# plt.yticks(np.arange(0, 3, .25))
 plt.title('Clay Target Motion')
-
-xvals = xvals[20:len(xvals)-20]
-yvals = yvals[20:len(yvals)-20]
-
-plt.plot(xvals, yvals, 'b', label='Truncated Area', linewidth=2)
 
 plt.legend()
 plt.grid()
